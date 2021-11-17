b7aff880760622393605128cea85ba38db186deb
garikAntonovich <garik.antonovich@gmail.com>
2021-11-17 18:31:38 +0300

    added another requestmapping to the controller and another page html

--- src/main/java/by/iharantanovich/thefirstserver/Application.java
+++ src/main/java/by/iharantanovich/thefirstserver/Application.java
@@ -1,12 +1,16 @@
 package by.iharantanovich.thefirstserver;
 
+import by.iharantanovich.thefirstserver.controller.FileUploadController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        new File(FileUploadController.uploadDirectory).mkdir();
         SpringApplication.run(Application.class, args);
     }
 
--- src/main/java/by/iharantanovich/thefirstserver/controller/FileUploadController.java
+++ src/main/java/by/iharantanovich/thefirstserver/controller/FileUploadController.java
@@ -3,6 +3,13 @@ package by.iharantanovich.thefirstserver.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class FileUploadController {
@@ -11,5 +18,23 @@ public class FileUploadController {
     @RequestMapping("/")
     public String uploadPage(Model model) {
         return "upload_view";
+    }
+
+    @RequestMapping("/upload")
+    public String upload(Model model, @RequestParam("files") MultipartFile[] files) {
+        StringBuilder fileNames = new StringBuilder();
+
+        for (MultipartFile file : files) {
+            Path fileNameAndPath = Paths.get(uploadDirectory, file.getOriginalFilename());
+            fileNames.append(file.getOriginalFilename() + " ");
+            try {
+                Files.write(fileNameAndPath, file.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        model.addAttribute("message", "Successfully uploaded files: " + fileNames.toString());
+        return "upload_status_view";
     }
 }
