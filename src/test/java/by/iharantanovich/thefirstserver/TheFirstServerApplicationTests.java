5b56bc689fcee63f43f6656f64fae80c5f1b5af2
garikAntonovich <garik.antonovich@gmail.com>
2021-11-17 16:07:07 +0300

    initial project

---
+++ src/test/java/by/iharantanovich/thefirstserver/TheFirstServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.iharantanovich.thefirstserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFirstServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
