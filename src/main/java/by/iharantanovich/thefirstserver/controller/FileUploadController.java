1dc9d045bdc7ff6b26aa9bcdaeab8af9c9cf960a
garikAntonovich <garik.antonovich@gmail.com>
2021-11-20 18:53:19 +0300

    the second web page was deleted and changes were made to the controller

--- src/main/java/by/iharantanovich/thefirstserver/controller/FileUploadController.java
+++ src/main/java/by/iharantanovich/thefirstserver/controller/FileUploadController.java
@@ -1,40 +1,23 @@
 package by.iharantanovich.thefirstserver.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Controller
 public class FileUploadController {
-    public static String uploadDirectory = System.getProperty("user.dir") + "/uploads";
 
-    @RequestMapping("/")
-    public String uploadPage(Model model) {
+    @GetMapping("/upload-view")
+    public String uploadPage() {
         return "upload_view";
     }
 
-    @RequestMapping("/upload")
-    public String upload(Model model, @RequestParam("files") MultipartFile[] files) {
-        StringBuilder fileNames = new StringBuilder();
-
-        for (MultipartFile file : files) {
-            Path fileNameAndPath = Paths.get(uploadDirectory, file.getOriginalFilename());
-            fileNames.append(file.getOriginalFilename() + " ");
-            try {
-                Files.write(fileNameAndPath, file.getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        model.addAttribute("message", "Successfully uploaded files: " + fileNames.toString());
-        return "upload_status_view";
+    @PostMapping("/upload-status")
+    @ResponseBody
+    public String uploadStatus(@RequestParam("file") MultipartFile file) {
+        return file.getOriginalFilename();
     }
 }
