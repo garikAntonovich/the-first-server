9f41ea17124b15c86151f12fecd23060673dee13
garikAntonovich <garik.antonovich@gmail.com>
2021-12-14 23:18:33 +0300

    changed controller

--- src/main/java/by/iharantanovich/thefirstserver/controller/FileUploadController.java
+++ src/main/java/by/iharantanovich/thefirstserver/controller/FileUploadController.java
@@ -1,7 +1,5 @@
 package by.iharantanovich.thefirstserver.controller;
 
-import by.iharantanovich.thefirstserver.model.ZippedFile;
-import by.iharantanovich.thefirstserver.service.DataTransferService;
 import by.iharantanovich.thefirstserver.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,20 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.Objects;
-
 @Controller
 public class FileUploadController {
 
-    protected FileUploadService fileUploadService;
-    protected DataTransferService dataTransferService;
-
     @Autowired
-    public FileUploadController(FileUploadService fileUploadService, DataTransferService dataTransferService) {
-        this.fileUploadService = fileUploadService;
-        this.dataTransferService = dataTransferService;
-    }
+    FileUploadService fileUploadService;
 
     @GetMapping("/upload")
     public String uploadPage() {
@@ -33,15 +22,12 @@ public class FileUploadController {
 
     @PostMapping("/upload")
     public String uploadStatus(@RequestParam("file") MultipartFile file, Model model) {
-
-        if (Objects.requireNonNull(file.getOriginalFilename()).endsWith(".zip")) {
-            fileUploadService.parseZippedFiles(fileUploadService.saveZippedFiles(file));
-            dataTransferService.transferData(fileUploadService.extractData());
-            model.addAttribute("message", "Successfully uploaded file: " + file.getOriginalFilename());
+        if (file.isEmpty()) {
+            model.addAttribute("fail", "File is empty...");
         } else {
-            model.addAttribute("fail", "This is not a zip file: " + file.getOriginalFilename());
+            model.addAttribute("message", "Successfully uploaded file: " + file.getOriginalFilename());
+            fileUploadService.processingFile(file);
         }
-
         return "upload_status_view";
     }
 }
