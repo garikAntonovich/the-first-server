70a0e95874d7c4101758c1106539c307a89e660e
garikAntonovich <garik.antonovich@gmail.com>
2021-12-15 00:23:12 +0300

    changed service

--- src/main/java/by/iharantanovich/thefirstserver/service/FileUploadServiceImpl.java
+++ src/main/java/by/iharantanovich/thefirstserver/service/FileUploadServiceImpl.java
@@ -5,7 +5,6 @@ import by.iharantanovich.thefirstserver.model.ZippedFile;
 import by.iharantanovich.thefirstserver.parser.jaxb.mainXmlFile.RootMain;
 import by.iharantanovich.thefirstserver.parser.jaxb.supplementaryXmlFile.RootSupplementary;
 import by.iharantanovich.thefirstserver.service.FileUploadService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,35 +20,37 @@ import java.util.zip.ZipInputStream;
 @Service
 public class FileUploadServiceImpl implements FileUploadService {
 
-    private static final String MAIN_XML = "</SKP_REPORT_KS>";
-    private static final String SUPPLEMENTARY_XML = "</Inf_Pay_Doc>";
-
-    protected List<ZippedFile> zippedFiles = new ArrayList<>();
+    private List<ZippedFile> zippedFileList;
     private RootMain rootMain;
     private RootSupplementary rootSupplementary;
 
     @Override
-    public List<ZippedFile> saveZippedFiles(MultipartFile file) {
+    public List<DataToTransfer> processingFile(MultipartFile file) {
+        saveZippedFiles(file);
+        parseZippedFiles(zippedFileList);
+        return extractData();
+    }
+
+    @Override
+    public void saveZippedFiles(MultipartFile file) {
 
         try (ZipInputStream zis = new ZipInputStream(file.getInputStream())) {
 
+            zippedFileList = new ArrayList<>();
             ZipEntry zipEntry;
 
             while ((zipEntry = zis.getNextEntry()) != null) {
                 if (zipEntry.getName().endsWith(".xml")) {
-                    zippedFiles.add(new ZippedFile(zipEntry.getName(), new String(zis.readNBytes((int) zipEntry.getSize()))));
+                    zippedFileList.add(new ZippedFile(zipEntry.getName(), new String(zis.readNBytes((int) zipEntry.getSize()))));
                 } else {
                     continue;
                 }
                 zis.closeEntry();
             }
 
-            return zippedFiles;
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return null;
     }
 
     @Override
@@ -59,7 +60,7 @@ public class FileUploadServiceImpl implements FileUploadService {
 
             for (ZippedFile zippedFile : zippedFiles) {
 
-                if (zippedFile.getData().endsWith(MAIN_XML)) {
+                if (zippedFile.getData().endsWith("</SKP_REPORT_KS>")) {
                     RootMain mainXml = (RootMain) JAXBContext.newInstance(RootMain.class).createUnmarshaller().
                             unmarshal(new StringReader(zippedFile.getData()));
                     if (!(mainXml.getReportTypeFlag().equals("Итоговая"))) {
@@ -67,7 +68,7 @@ public class FileUploadServiceImpl implements FileUploadService {
                     } else {
                         rootMain = mainXml;
                     }
-                } else if (zippedFile.getData().endsWith(SUPPLEMENTARY_XML)) {
+                } else if (zippedFile.getData().endsWith("</Inf_Pay_Doc>")) {
                     rootSupplementary = (RootSupplementary) JAXBContext.newInstance(RootSupplementary.class).createUnmarshaller().
                             unmarshal(new StringReader(zippedFile.getData()));
                 }
@@ -78,23 +79,22 @@ public class FileUploadServiceImpl implements FileUploadService {
         }
     }
 
+    @Override
     public List<DataToTransfer> extractData() {
 
         List<DataToTransfer> dataToTransferList = new ArrayList<>();
 
         for (int index = 0; index < rootMain.getDoc().size(); index++) {
             if (rootMain.getDoc().get(index).getDocGUID().equals(rootSupplementary.getDoc().get(index).getGuid())) {
-                DataToTransfer dataToTransfer = new DataToTransfer();
-                dataToTransfer.setDocNum(rootMain.getDoc().get(index).getDocNum());
-                dataToTransfer.setDocDate(rootMain.getDoc().get(index).getDocDate());
-                dataToTransfer.setDocGUID(rootMain.getDoc().get(index).getDocGUID());
-                dataToTransfer.setOperType(rootMain.getDoc().get(index).getOperType());
-                dataToTransfer.setAmountOut(rootMain.getDoc().get(index).getAmountOut());
-                dataToTransfer.setInfPay(rootSupplementary.getDoc().get(index).getInfPay());
-                dataToTransfer.setBankPay(rootSupplementary.getDoc().get(index).getBankPay());
-                dataToTransfer.setInfRcp(rootSupplementary.getDoc().get(index).getInfRcp());
-                dataToTransfer.setBankRcp(rootSupplementary.getDoc().get(index).getBankRcp());
-                dataToTransfer.setPurpose(rootSupplementary.getDoc().get(index).getPurpose());
+                DataToTransfer dataToTransfer = new DataToTransfer(
+                        rootMain.getDoc().get(index).getDocNum(), rootMain.getDoc().get(index).getDocDate(),
+                        rootMain.getDoc().get(index).getDocGUID(), rootMain.getDoc().get(index).getOperType(),
+                        rootMain.getDoc().get(index).getAmountOut(),
+                        rootSupplementary.getDoc().get(index).getInfPay(),
+                        rootSupplementary.getDoc().get(index).getBankPay(),
+                        rootSupplementary.getDoc().get(index).getInfRcp(),
+                        rootSupplementary.getDoc().get(index).getBankRcp(),
+                        rootSupplementary.getDoc().get(index).getPurpose());
                 dataToTransferList.add(dataToTransfer);
             }
         }
